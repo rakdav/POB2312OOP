9f9b05d2368004075ab30c3e3f4821bd9ab9fe7f
Aleksey Burov <burovalexprogrammer@gmail.com>
2024-06-29 19:48:06 +0200

    Lesson9.1

---
+++ Lesson9/MyExtension.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Lesson9
+{
+     public static class MyExtension
+     {
+            public static bool IsValidEMailAddress(this string s)
+            {
+                Regex regex = new Regex(@"^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$");
+                return regex.IsMatch(s);
+            }
+        public static int WordCount(this string s,char symbol)
+        {
+            int count = 0;
+            for (int i = 0; i < s.Length; i++)
+            {
+                if (s[i] == symbol) count++;
+            }
+            return count;
+        }
+     }
+}
---
+++ Lesson9/Program.cs
@@ -0,0 +1,62 @@
+﻿using Lesson9;
+using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+string myEmail = "resr@mail.ru";
+Console.WriteLine(myEmail.IsValidEMailAddress());
+Console.WriteLine(myEmail.WordCount('r'));
+
+string main = "Java";
+string second = "C#";
+Console.WriteLine(main+" "+second);
+(main, second) = (second, main);
+Console.WriteLine(main + " " + second);
+
+Random random = new Random();
+int[] mas = new int[random.Next(5,10)];
+for (int i = 0; i < mas.Length; i++)
+{
+    mas[i] = random.Next(10, 100);
+    Console.Write(mas[i]+" ");
+}
+Console.WriteLine();
+for (int i = 0; i < mas.Length-1; i++)
+{
+    for(int j = i + 1; j < mas.Length; j++)
+    {
+        if (mas[i] > mas[j])
+        {
+            (mas[i], mas[j]) = (mas[j], mas[i]);
+        }
+    }
+}
+for (int i = 0; i < mas.Length; i++)
+{
+    Console.Write(mas[i] + " ");
+}
+var tuple = GetValues();
+Console.WriteLine(tuple.Item1);
+Console.WriteLine(tuple.Item2);
+var masTuple = getMasParametrs(mas);
+Console.WriteLine(masTuple.Item1);
+Console.WriteLine(masTuple.Item2);
+Console.WriteLine(masTuple.Item3);
+Console.WriteLine(masTuple.Item4);
+(int,int) GetValues()
+{
+    var result = (3, 9);
+    return result;
+}
+(int,int,int,double) getMasParametrs(int[] mas)
+{
+    var results = (sum: 0, max: int.MinValue,
+        min: int.MaxValue, avg: 0.0);
+    for(int i = 0; i < mas.Length; i++)
+    {
+        results.sum += mas[i];
+    }
+    results.min = mas.Min();
+    results.max = mas.Max();
+    results.avg = mas.Average();
+    return results;
+}
